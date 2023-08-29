7548e692241825b863ec505b85f11f0832c3a718
shafi <shazzmohammed08@gmail.com>
2023-08-29 11:35:07 +0530

    host bug3

--- ms/settings.py
+++ ms/settings.py
@@ -145,10 +145,15 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = config('DEFAULT_AUTO_FIELD')
-EMAIL_BACKEND = config('EMAIL_BACKEND')
-EMAIL_HOST = config('EMAIL_HOST')  # e.g., smtp.gmail.com
+# EMAIL_BACKEND = config('EMAIL_BACKEND')
+# EMAIL_HOST = config('EMAIL_HOST')  # e.g., smtp.gmail.com
+# EMAIL_PORT = 587  # check your SMTP provider for the correct port
+# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'  # e.g., smtp.gmail.com
 EMAIL_PORT = 587  # check your SMTP provider for the correct port
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = 'shazzmohammed08@gmail.com'
+EMAIL_HOST_PASSWORD = 'ogezaoagatiecbev'
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
